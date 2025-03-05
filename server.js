01abbac79a9f4f2022b5745acd73fce404bc0c12
Jalal <jalal231@hotmail.com>
2025-03-05 15:12:33 +0530

    changed file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-	    res.send('Hello, Azure Web App!');
+	    res.send('Hello, Ibrahim!');
 });
 
 app.listen(port, () => {
