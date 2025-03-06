22ebef3cc424a0f5fa255f6cc312b07e9e9ae777
Jalal <jalal231@hotmail.com>
2025-03-06 11:49:53 +0530

    Corrected json

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-	    res.send('Hello, This is Jalaluddin Mohammed!');
+	    res.send('Hello,... This is Jalaluddin Mohammed!');
 });
 
 app.listen(port, () => {
