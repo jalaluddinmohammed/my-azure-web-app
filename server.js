10b3fc1f21c9db9dd1b0210eb1b2ab6a6fda10b0
Jalal <jalal231@hotmail.com>
2025-03-06 10:58:55 +0530

    Re added credentials

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-	    res.send('Hello, This is Jalal!');
+	    res.send('Hello, This is Jalaluddin!');
 });
 
 app.listen(port, () => {
